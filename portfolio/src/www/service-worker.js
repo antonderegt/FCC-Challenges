32bf64b499a98466f7a89d0692710c12086795a1
Anton de Regt <antonderegt@gmail.com>
2017-01-22 15:23:45 +0100

    Added ServiceWorkers

---
+++ portfolio/src/www/service-worker.js
@@ -0,0 +1,6 @@
+const CACHE_NAME='v1'
+const CACHE_FILES=['./','./manifest.json','./index.html','./app.js','./images/100.png','./images/fcc1.png','./images/nac.png','./images/photo-anton.png','./images/talkabout.png']
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(cache=>{return cache.addAll(CACHE_FILES)}))})
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(cacheNames=>{return Promise.all(cacheNames.map(thisCacheName=>{if(thisCacheName!==CACHE_NAME){return caches.delete(thisCacheName)}}))}))})
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(response=>{if(response){return response}var fetchRequest=e.request.clone();return fetch(fetchRequest).then(response=>{if(!response){return response}
+var responseToCache=response.clone();caches.open(CACHE_NAME).then(cache=>{cache.put(e.request,responseToCache)});return response})}).catch(err=>{}))})
