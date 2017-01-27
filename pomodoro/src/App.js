5e4a356258f381214618f957d8470ee05aaf76db
Anton de Regt <antonderegt@gmail.com>
2017-01-27 22:19:11 +0100

    Build basic timer capability

--- pomodoro/src/App.js
+++ pomodoro/src/App.js
@@ -2,31 +2,79 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const START = "START",
+      BREAK = "BREAK",
+      STOPPED = "STOPPED";
+
 class App extends Component {
-  timer() {
+  constructor(props) {
+    super(props)
+    this.state = {
+      timer: "0h 25m 0s",
+      text: "Start Pomodoro",
+      workTime: 5000,
+      breakTime: 5000,
+      state: START
+    }
+  }
+
+  setTimer(workTime, breakTime) {
+    this.setState({
+      workTime,
+      breakTime
+    })
+  }
+
+  startTimer(time) {
     // Set the date we're counting down to
-    var countDownDate = new Date().getTime()+1500000;//new Date("Jan 5, 2018 15:37:25").getTime();
-    // Update the count down every 1 second
-    var x = setInterval(function() {
+    var countDownDate = new Date().getTime()+time;//1500000;//new Date("Jan 5, 2018 15:37:25").getTime();
+
+    if(this.state.state === START) {
+      this.setState({
+        text: "Timer running"
+      })
+    } else if(this.state.state === BREAK) {
+      this.setState({
+        text: "Take a break"
+      })
+    }
+
+    let interval = setInterval(() => {
       // Get todays date and time
-      var now = new Date().getTime();
+      let now = new Date().getTime();
       // Find the distance between now an the count down date
-      var distance = countDownDate - now;
+      let distance = countDownDate - now;
       // Time calculations for days, hours, minutes and seconds
-      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+      let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      let seconds = Math.floor((distance % (1000 * 60)) / 1000);
       // Display the result in the element with id="demo"
-      document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-      + minutes + "m " + seconds + "s ";
+      let newTime = hours + "h " + minutes + "m " + seconds + "s ";
+      this.setState({
+        timer: newTime,
+      })
       // If the count down is finished, write some text
       if (distance < 0) {
-        clearInterval(x);
-        document.getElementById("demo").innerHTML = "EXPIRED";
+        if(this.state.state === START) {
+          this.setState({
+            timer: "0h 0m 0s",
+            text: "Break",
+            state: BREAK
+          })
+          clearInterval(interval);
+          this.startTimer(this.state.breakTime);
+        } else if (this.state.state === BREAK) {
+          this.setState({
+            timer: "0h 0m 0s",
+            text: "Restart timer",
+            state: STOPPED
+          })
+          clearInterval(interval);
+        }
       }
-    }, 1000);
+    }, 1000)
   }
+
   render() {
     return (
       <div className="App">
@@ -34,10 +82,12 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Reactive Pomodoro</h2>
         </div>
-        <p className="App-intro">
-          Timer is running...
-        </p>
-        <div id="demo">{this.timer()}</div>
+        <div>
+          <p>Timer is set for 25 minutes.</p>
+          <button onClick={() => {this.startTimer(5000)}}>{this.state.text}</button>
+          <div>{this.state.timer}</div>
+          <button onClick={() => {this.setTimer(5000, 3000)}}>Set 5s 3s</button>
+        </div>
       </div>
     );
   }
