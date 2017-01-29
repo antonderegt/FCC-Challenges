ebd68580e534c36f8aadb72ec4ea9a9d913f8cb5
Anton de Regt <antonderegt@gmail.com>
2017-01-29 19:37:01 +0100

    Added simple styling

--- pomodoro/src/App.js
+++ pomodoro/src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-// @TODO Add pauze button
+// @TODO Add pause button
 
 const BREAK = "BREAK",
       STOPPED = "STOPPED";
@@ -10,20 +9,20 @@ const BREAK = "BREAK",
 class App extends Component {
   constructor(props) {
     super(props)
-    let newTimer = this.calcTime(5000)
+    let newTimer = this.calcTime(25*60000)
     this.state = {
       timer: newTimer,
       text: "Start Pomodoro",
-      workTime: 5,
-      breakTime: 3,
+      workTime: 25,
+      breakTime: 5,
       state: STOPPED,
       disabled: false
     }
   }
 
   setWorkTime(time) {
-    let newWT = this.state.workTime+time
-    let newTimer = this.calcTime(newWT*1000)
+    let newWT = this.state.workTime+(time)
+    let newTimer = this.calcTime(newWT*60000)
     if(newWT > 0) {
       this.setState({
         workTime: newWT,
@@ -33,7 +32,7 @@ class App extends Component {
   }
 
   setBreakTime(time) {
-    let newBT = this.state.breakTime+time
+    let newBT = this.state.breakTime+(time)
     if(newBT > 0) {
       this.setState({
         breakTime: newBT
@@ -49,57 +48,47 @@ class App extends Component {
   }
 
   startTimer(time) {
+    if(this.state.state === STOPPED && this.state.text !== "Timer running") {
+      this.setState({
+        text: "Timer running",
+        disabled: true
+      })
+    }
+
     this.setState({
       disabled: true
     })
 
-    switch (this.state.state) {
-      case STOPPED:
-        this.setState({
-          text: "Timer running"
-        })
-        break;
-      case BREAK:
-        this.setState({
-          text: "Take a break"
-        })
-        break;
-      default:
-    }
-
-    var countDownDate = new Date().getTime()+(time*1000)+1000;
+    var countDownDate = performance.now()+(time*60000)+1000;
 
     let interval = setInterval(() => {
-      // Get todays date and time
-      let now = new Date().getTime();
-      // Find the distance between now an the count down date
-      let distance = countDownDate - now;
-      // Time calculations for days, hours, minutes and seconds
+      let now = performance.now()
+      let distance = countDownDate - now
       let newTime = this.calcTime(distance)
 
       this.setState({
-        timer: newTime,
+        timer: newTime
       })
-      // If the count down is finished, write some text
+
+      // Count down is finished
       if (distance < 0) {
         if(this.state.state === STOPPED) {
           clearInterval(interval);
-          let newTimer = this.calcTime(this.state.breakTime*1000)
+          let newTimer = this.calcTime(this.state.breakTime*60000)
           this.setState({
             timer: newTimer,
-            state: BREAK
+            state: BREAK,
+            text: "Take a break"
           })
-
-          this.startTimer(this.state.breakTime);
+          this.startTimer(this.state.breakTime)
         } else if (this.state.state === BREAK) {
-          clearInterval(interval);
+          clearInterval(interval)
           this.setState({
             timer: "0h 0m 0s",
             text: "Restart timer",
             state: STOPPED,
             disabled: false
           })
-
         }
       }
     }, 1000)
@@ -108,15 +97,30 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Reactive Pomodoro</h2>
-        </div>
-        <div>
-          <p>Session Length</p><button onClick={() => this.setWorkTime(1)} disabled={this.state.disabled}>+</button><p>{this.state.workTime}</p><button onClick={() => this.setWorkTime(-1)} disabled={this.state.disabled}>-</button>
-          <p>Break Length</p><button onClick={() => this.setBreakTime(1)} disabled={this.state.disabled}>+</button><p>{this.state.breakTime}</p><button onClick={() => this.setBreakTime(-1)} disabled={this.state.disabled}>-</button>
-          <p>Timer</p><button onClick={() => {this.startTimer(this.state.workTime)}} disabled={this.state.disabled}>{this.state.text}</button>
-          <div>{this.state.timer}</div>
+        <div className="flexContainer">
+          <h1>Reactive Pomodoro</h1>
+          <div className="flexRow">
+
+            <div className="flexColumn">
+              <h3>Session Length</h3>
+              <div className="flexRow">
+                <button onClick={() => this.setWorkTime(1)} disabled={this.state.disabled}><b>+</b></button>
+                {this.state.workTime}<button onClick={() => this.setWorkTime(-1)} disabled={this.state.disabled}><b>-</b></button>
+              </div>
+            </div>
+
+            <div className="flexColumn">
+              <h3>Break Length</h3>
+              <div className="flexRow">
+                <button onClick={() => this.setBreakTime(1)} disabled={this.state.disabled}><b>+</b></button>
+                {this.state.breakTime}<button onClick={() => this.setBreakTime(-1)} disabled={this.state.disabled}><b>-</b></button>
+              </div>
+            </div>
+
+          </div>
+            <h3>Pomodoro</h3>
+              <button onClick={() => {this.startTimer(this.state.workTime)}} disabled={this.state.disabled}><b>{this.state.text}</b></button>
+            <h2>{this.state.timer}</h2>
         </div>
       </div>
     );
