b822c31fed1f49b49e6cdc22957346b5d1fe6b38
Anton de Regt <antonderegt@gmail.com>
2017-02-07 19:23:36 +0100

    Readded sw

--- portfolio/src/app/app.js
+++ portfolio/src/app/app.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import {render} from 'react-dom'
 import injectTapEventPlugin from 'react-tap-event-plugin'
-// import Sw from './sw'
-import Main from './Main' // Our custom react component
+import Sw from './sw'
+import Main from './Main'
 
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
